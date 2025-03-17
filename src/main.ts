a2d647d53ba6a7bc5cbd5db7600e507b81aed9af
Aldoivan <aldoivan_palma@hotmail.com>
2025-03-16 23:17:20 -0600

    Setup: Quasar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/es'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,24 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    config: {
+        brand: {
+            primary: '#ffc107',
+            secondary: '#ff9800',
+            accent: '#9C27B0',
+
+            dark: '#333333',
+            'dark-page': '#1d1d1d',
+
+            positive: '#21BA45',
+            negative: '#f44336',
+            info: '#448aff',
+            warning: '#fec408'
+        }
+    },
+    lang: quasarLang,
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls }
+    }),
     vueDevTools(),
+    quasar()
   ],
   resolve: {
     alias: {
