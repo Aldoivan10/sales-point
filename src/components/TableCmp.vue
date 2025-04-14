0433f68c577f75cbafa4ac6c11bbd222ca27362a
Aldoivan <aldoivan_palma@hotmail.com>
2025-04-13 22:47:30 -0600

    Add: Keyboard navigation

--- src/components/TableCmp.vue
+++ src/components/TableCmp.vue
@@ -1,86 +1,148 @@
 <script setup lang="ts">
-import type { QTable, QTableColumn, QTableProps } from 'quasar'
-import { onMounted, ref } from 'vue'
+import type { QCheckbox, QTable, QTableColumn, QTableProps } from 'quasar'
+import { nextTick, onMounted, ref, watch } from 'vue'
 
 const {
-    icon = '',
-    rows = [],
-    columns = [],
-    title = 'Tabla',
-    loading = false,
+	icon = '',
+	rows = [],
+	columns = [],
+	title = 'Tabla',
+	loading = false,
+	select = false,
 } = defineProps<{
-    rows?: any[]
-    title?: string
-    loading?: boolean
-    columns?: QTableColumn[]
-    icon?: string
+	rows?: any[]
+	title?: string
+	loading?: boolean
+	columns?: QTableColumn[]
+	icon?: string
+	select?: boolean
 }>()
 
 const $table = ref<QTable>()
+const selected = ref<number[]>([])
+const $checks = ref<HTMLInputElement[]>([])
 const filter = defineModel('filter', { type: String })
-const pagination = defineModel('pagination', { type: Object as () => QTableProps['pagination'] })
+const pagination = defineModel('pagination', {
+	type: Object as () => QTableProps['pagination'],
+})
 
-onMounted(() => $table.value?.requestServerInteraction())
+/* function highlightRow(index = -1, { page, rowsPerPage }: QTableProps['pagination'], next = 1) {
+	const $checkbox = $checks.value[index + next]
+	const len = $checks.value.length
+	if ($checkbox) $checkbox.focus()
+	else $checks.value[next == 1 ? 0 : len - 1].focus()
+} */
+
+function highlightRow(
+	{ page = 1, rowsPerPage = 50 }: QTableProps['pagination'],
+	index = -1,
+	next = 1,
+) {
+	console.log(page, rowsPerPage, index)
+	const offset = (page - 1) * rowsPerPage
+	const checkIndex = index + next - offset
+	const $checkbox = $checks.value[checkIndex]
+	const len = $checks.value.length
+	if ($checkbox) $checkbox.focus()
+	else $checks.value[next == 1 ? 0 : len - 1].focus()
+}
+
+watch(
+	() => rows,
+	async () => {
+		await nextTick()
+		$checks.value = Array.from(document.querySelectorAll('.q-checkbox'))
+	},
+)
+
+onMounted(async () => $table.value?.requestServerInteraction())
 </script>
 
 <template>
-    <q-table
-        :title
-        :rows
-        :columns
-        :loading
-        ref="$table"
-        row-key="id"
-        virtual-scroll
-        color="primary"
-        v-model:pagination="pagination"
-        :rows-per-page-options="[50, 100, 250, 500, 0]"
-        table-header-class="[&>.text-left]:!text-center"
-        @request="(props) => (pagination = props.pagination)"
-        class="overflow-auto [&_thead]:sticky [&_thead]:z-10 [&_thead]:top-0 [&_tbody_tr]:odd:!bg-gray-100 [&_thead]:!bg-primary [&_thead]:!text-white [&_thead_th]:!font-bold [&>.q-table\_\_top]:!bg-primary [&>.q-table\_\_top]:!text-white [&_.q-table\_\_bottom]:uppercase"
-    >
-        <template v-slot:top>
-            <div class="flex gap-2 items-center w-full">
-                <q-icon :name="icon" size="2em"></q-icon>
-                <h6 class="m-0 h-fit w-35">{{ title }}</h6>
-                <q-input
-                    dense
-                    filled
-                    color="white"
-                    debounce="300"
-                    v-model="filter"
-                    label-color="white"
-                    placeholder="BUSCAR..."
-                    input-class="focus:!text-white !text-orange-200 !uppercase"
-                    class="[&:focus-within_.q-icon]:text-white grow [&_.q-icon]:text-orange-200 [&_.q-field\_\_control]:before:!border-orange-200"
-                >
-                    <template v-slot:append>
-                        <q-icon name="r_search" />
-                    </template>
-                </q-input>
-                <slot name="header-append"></slot>
-            </div>
-        </template>
-        <template v-slot:body="props">
-            <q-tr :props="props">
-                <template v-for="col in columns" :key="col.name">
-                    <template v-if="$slots[`body-cell-${col.name}`]">
-                        <slot :name="`body-cell-${col.name}`" v-bind="props" />
-                    </template>
-                    <template v-else>
-                        <q-td :key="col.name" :props="props">
-                            {{
-                                typeof col.field == 'function'
-                                    ? col.field(props.row)
-                                    : props.row[String(col.field)]
-                            }}
-                        </q-td>
-                    </template>
-                </template>
-            </q-tr>
-        </template>
-        <template v-slot:loading>
-            <q-inner-loading showing color="primary" />
-        </template>
-    </q-table>
+	<q-table
+		:title
+		:rows
+		:columns
+		:loading
+		use-input
+		ref="$table"
+		row-key="id"
+		color="primary"
+		v-model:selected="selected"
+		v-model:pagination="pagination"
+		:selection="select ? 'multiple' : 'none'"
+		:rows-per-page-options="[10, 50, 100, 250, 500, 0]"
+		@request="(props) => (pagination = props.pagination)"
+		table-header-class="[&>.text-left]:!text-center [&_th]:!font-bold bg-primary text-white z-10 top-0 sticky"
+		class="overflow-auto [&>.q-table\_\_top]:!bg-primary [&>.q-table\_\_top]:!text-white [&_.q-table\_\_bottom]:uppercase"
+	>
+		<template v-slot:header-selection="props">
+			<q-checkbox
+				class="[&:is([aria-checked=true],[aria-checked=mixed])_svg]:!text-secondary [&:is([aria-checked=true],[aria-checked=mixed])_.q-checkbox\_\_bg]:!bg-white [&:is([aria-checked=true],[aria-checked=mixed])_.q-checkbox\_\_bg]:!border-white"
+				@keydown.up.prevent="highlightRow(pagination, 0, -1)"
+				@keydown.down.prevent="highlightRow(pagination, 0)"
+				v-model="props.selected"
+			/>
+		</template>
+		<template v-slot:top>
+			<div class="flex gap-2 items-center w-full">
+				<q-icon :name="icon" size="2em"></q-icon>
+				<h6 class="m-0 h-fit w-35">{{ title }}</h6>
+				<q-input
+					dense
+					filled
+					color="white"
+					debounce="300"
+					v-model="filter"
+					label-color="white"
+					placeholder="BUSCAR..."
+					@keydown.down.prevent="highlightRow(pagination)"
+					@keydown.up.prevent="highlightRow(pagination, -1, -1)"
+					v-shortcut.prevent.click="['ctrl+shift+space']"
+					input-class="focus:!text-white !text-orange-200 !uppercase"
+					class="[&:focus-within_.q-icon]:text-white grow [&_.q-icon]:text-orange-200 [&_.q-field\_\_control]:before:!border-orange-200"
+				>
+					<template v-slot:append>
+						<q-icon name="r_search" />
+					</template>
+				</q-input>
+				<slot name="header-append"></slot>
+			</div>
+		</template>
+		<template v-slot:body="props">
+			<q-tr
+				class="[&:has([aria-checked=true])]:!bg-secondary/10 [&:focus-within]:!bg-secondary/5 [&:not(:has([aria-checked=true]))]:odd:bg-gray-100"
+				:props="props"
+			>
+				<q-td v-if="select" auto-width>
+					<q-checkbox
+						@keydown.down.prevent="
+							highlightRow(pagination, props.rowIndex + 1)
+						"
+						@keydown.up.prevent="
+							highlightRow(pagination, props.rowIndex + 1, -1)
+						"
+						v-model="props.selected"
+					/>
+				</q-td>
+				<template v-for="col in columns" :key="col.name">
+					<template v-if="$slots[`body-cell-${col.name}`]">
+						<slot :name="`body-cell-${col.name}`" v-bind="props" />
+					</template>
+					<template v-else>
+						<q-td :key="col.name" :props="props">
+							{{
+								typeof col.field == 'function'
+									? col.field(props.row)
+									: props.row[String(col.field)]
+							}}
+						</q-td>
+					</template>
+				</template>
+			</q-tr>
+		</template>
+		<template v-slot:loading>
+			<q-inner-loading showing color="primary" />
+		</template>
+	</q-table>
 </template>
