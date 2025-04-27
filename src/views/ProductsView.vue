0d2233d53dc0910e738d8ff7d81108e9720dac7c
Aldoivan <aldoivan_palma@hotmail.com>
2025-04-27 08:02:51 -0600

    Removed unused multiple fetching

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -4,16 +4,18 @@ import { useProductStore } from '@/stores/product.store'
 import { errorToast } from '@/utils/msg.util'
 import { storeToRefs } from 'pinia'
 import { useQuasar } from 'quasar'
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
+
+const { admin = false } = defineProps<{ admin?: boolean }>()
 
 const productStore = useProductStore()
 const {
-    columns,
-    products: rows,
-    pagination,
-    search: filter,
-    loading,
-    error,
+	columns,
+	products: rows,
+	pagination,
+	search: filter,
+	loading,
+	error,
 } = storeToRefs(productStore)
 const $q = useQuasar()
 const showKits = ref(false)
@@ -21,35 +23,36 @@ const tooltip = computed(() => (showKits.value ? 'PRODUCTOS' : 'KITS'))
 const title = computed(() => (showKits.value ? 'KITS' : 'PRODUCTOS'))
 
 watch(error, (newErrror) => {
-    if (newErrror) errorToast($q, newErrror.msg, newErrror.id)
+	if (newErrror) errorToast($q, newErrror.msg, newErrror.id)
 })
-
-onMounted(productStore.filter)
 </script>
 
 <template>
-    <TableCmp
-        :rows
-        :columns
-        :loading
-        :title
-        class="grow w-full"
-        icon="r_point_of_sale"
-        v-model:filter="filter"
-        v-model:pagination="pagination"
-    >
-        <template #header-append>
-            <q-toggle
-                unchecked-icon="r_inventory_2"
-                checked-icon="r_all_inbox"
-                v-model="showKits"
-                color="black"
-                size="lg"
-            >
-                <q-tooltip class="text-center">
-                    {{ tooltip }}
-                </q-tooltip>
-            </q-toggle>
-        </template>
-    </TableCmp>
+	<TableCmp
+		:rows
+		:title
+		:columns
+		:loading
+		:admin
+		select
+		class="grow w-full"
+		icon="r_point_of_sale"
+		v-model:filter="filter"
+		v-model:pagination="pagination"
+		@edit="(item) => console.log(item)"
+	>
+		<template #header-append>
+			<q-toggle
+				unchecked-icon="r_inventory_2"
+				checked-icon="r_all_inbox"
+				v-model="showKits"
+				color="black"
+				size="lg"
+			>
+				<q-tooltip class="text-center">
+					{{ tooltip }}
+				</q-tooltip>
+			</q-toggle>
+		</template>
+	</TableCmp>
 </template>
