dbc862dd282085545bc623331a41a64ca0a0c571
Aldoivan <aldoivan_palma@hotmail.com>
2025-04-27 08:01:53 -0600

    Update: Tab indent

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useQuasar } from 'quasar'
+import ChoiceUserDialog from './components/dialog/ChoiceUserDialog.vue'
 import HeaderLayout from './layout/HeaderLayout.vue'
 import NavLayout from './layout/NavLayout.vue'
-import ChoiceUserDialog from './components/dialog/ChoiceUserDialog.vue'
 import { useTabStore } from './stores/tab.store'
 import { okToast } from './utils/msg.util'
 
@@ -10,25 +10,30 @@ const $q = useQuasar()
 const tabStore = useTabStore()
 
 $q.dialog({
-    component: ChoiceUserDialog,
-    componentProps: {
-        persistent: true,
-    },
+	component: ChoiceUserDialog,
+	componentProps: {
+		persistent: true,
+	},
 }).onOk((user: User.State) => {
-    tabStore.addTab(user)
-    okToast($q, `ACCEDIDO COMO <i><b>${user.name}</b></i>`)
+	tabStore.addTab(user)
+	okToast($q, `ACCEDIDO COMO <i><b>${user.name}</b></i>`)
 })
 </script>
 
 <template>
-    <q-layout view="hhh lpR lFr" class="selection:text-white selection:bg-secondary">
-        <HeaderLayout />
-        <NavLayout />
+	<q-layout
+		view="hhh lpR lFr"
+		class="selection:text-white selection:bg-secondary"
+	>
+		<HeaderLayout />
+		<NavLayout />
 
-        <q-page-container class="h-dvh">
-            <main class="h-full p-4 place-items-center !flex flex-col justify-center items-center">
-                <router-view />
-            </main>
-        </q-page-container>
-    </q-layout>
+		<q-page-container class="h-dvh">
+			<main
+				class="h-full p-4 place-items-center !flex flex-col justify-center items-center"
+			>
+				<router-view />
+			</main>
+		</q-page-container>
+	</q-layout>
 </template>
