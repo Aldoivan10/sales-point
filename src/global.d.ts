92de0454b6a8dd93af70b87094297147835f0687
Aldoivan <aldoivan_palma@hotmail.com>
2025-04-27 08:00:09 -0600

    Create: Types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,114 +1,123 @@
 import 'vue'
 
 declare global {
-    namespace Route {
-        type Link = { path: string; name: string }
+	namespace Route {
+		type Link = { path: string; name: string }
 
-        type Links = Record<string, Link>
-    }
+		type Links = Record<string, Link>
+	}
 
-    namespace Menu {
-        type Item = {
-            title: string
-            subtitle: string
-            icon: string
-            admin: boolean
-            tooltip: string
-            route: string
-        }
-    }
+	namespace Menu {
+		type Item = {
+			title: string
+			subtitle: string
+			icon: string
+			admin: boolean
+			tooltip: string
+			route: string
+		}
+	}
 
-    namespace User {
-        type Shortcut = {
-            action: string
-            shortcut: string
-        }
+	namespace User {
+		type Shortcut = {
+			action: string
+			shortcut: string
+		}
 
-        type State = {
-            name: string
-            logged: boolean
-            views: Shortcut[]
-            shortcuts: Shortcut[]
-            route?: string
-            search?: string
-            itemsPerPage?: number
-            page?: number
-        }
+		type State = {
+			name: string
+			logged: boolean
+			views: Shortcut[]
+			shortcuts: Shortcut[]
+			route?: string
+			search?: string
+			itemsPerPage?: number
+			page?: number
+		}
 
-        type Login = { username: string; password: string }
-    }
+		type Login = { username: string; password: string }
+	}
 
-    namespace Product {
-        type Catalog = { id: number; name: string }
+	namespace Product {
+		type Catalog = { id: number; name: string }
 
-        type Code = Catalog & { code: string | null }
+		type Code = Catalog & { code: string | null }
 
-        type Unit = Catalog & { profit: number | null; sale: number }
+		type Unit = Catalog & { profit: number | null; sale: number }
 
-        type Item = {
-            id: number
-            name: string
-            amount: number
-            min: number
-            refundable: boolean
-            buy: number
-            supplier: Catalog
-            department: Catalog
-            codes: Code[]
-            units: Unit[]
-        }
-    }
+		type Item = {
+			id: number
+			name: string
+			amount: number
+			min: number
+			refundable: boolean
+			buy: number
+			supplier: Catalog
+			department: Catalog
+			codes: Code[]
+			units: Unit[]
+		}
+	}
 
-    namespace Table {
-        type Options = Partial<Pagination> & { search?: string }
+	namespace Table {
+		type Options = Partial<Pagination> & { search?: string }
 
-        type Pagination = {
-            page: number
-            rowsNumber: number
-            rowsPerPage: number
-        }
+		type Pagination = {
+			page: number
+			rowsNumber: number
+			rowsPerPage: number
+		}
 
-        type Entity = Options & { type?: string }
-    }
+		type Entity = Options & { type?: string }
+	}
 
-    namespace Form {
-        type Rule = (value: string) => boolean | string
-    }
+	namespace Form {
+		type Rule = (value: string) => boolean | string
+	}
 
-    namespace Entity {
-        type Item = {
-            rfc: string | null
-            id_entity_type: number
-            name: string
-            address: string | null
-            domicile: string | null
-            postal_code: string | null
-            phone: string | null
-            email: string | null
-            type: { id: number; name: string }
-        }
-    }
+	namespace Entity {
+		type Item = {
+			id: number
+			rfc: string | null
+			id_entity_type: number
+			name: string
+			address: string | null
+			domicile: string | null
+			postal_code: string | null
+			phone: string | null
+			email: string | null
+			type: { id: number; name: string }
+		}
+	}
 
-    type APIFech<T> = { items: T[]; total: number }
+	namespace Kit {
+		type Item = {
+			id: number
+			name: string
+			products: Product.Item[]
+		}
+	}
 
-    type APIParams = { limit?: number; offset?: number } & Record<string, any>
+	type APIFech<T> = { items: T[]; total: number }
 
-    type Role = 'info' | 'warning' | 'error' | 'success'
+	type APIParams = { limit?: number; offset?: number } & Record<string, any>
 
-    type Maybe<T> = null | undefined | T
+	type Role = 'info' | 'warning' | 'error' | 'success'
 
-    type APIError = { id: string; msg: string }
+	type Maybe<T> = null | undefined | T
 
-    type Env = ImportMetaEnv & {
-        VITE_API_URL: string
-    }
+	type APIError = { id: string; msg: string }
 
-    type Head = {
-        key?: string
-        title?: string
-        sortable?: boolean
-        value: SelectItemKey<T>
-    }
+	type Env = ImportMetaEnv & {
+		VITE_API_URL: string
+	}
+
+	type Head = {
+		key?: string
+		title?: string
+		sortable?: boolean
+		value: SelectItemKey<T>
+	}
 }
 
 export {}
