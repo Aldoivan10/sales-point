1028bf7962ba05e573b84fc4710697e578a03c38
Aldoivan <aldoivan_palma@hotmail.com>
2025-04-27 07:59:49 -0600

    Update: Iniatial configuration

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
+import '@/assets/main.css'
 import '@quasar/extras/material-icons-round/material-icons-round.css'
 import 'quasar/src/css/index.sass'
-import '@/assets/main.css'
 
+import { ApiCLientPlugin } from '@aldoivan10/v-api-client'
+import { VShortcut } from '@aldoivan10/v-shortcut'
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
-import { ApiCLientPlugin } from '@aldoivan10/v-api-client'
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import router from './router'
 import { theme, VITE_API_URL } from './config'
+import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VShortcut)
 app.use(ApiCLientPlugin, { baseUrl: VITE_API_URL })
 app.use(Quasar, theme)
 
